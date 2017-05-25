4b2b63c9d94434321b8ea500eee9339b0154d329
Jan Freyberg <jan.freyberg@gmail.com>
2017-05-25 17:48:49 +0100

    Make little toy version!

--- rivalry.py
+++ rivalry.py
@@ -12,10 +12,9 @@ trialdur = 5.0  # trial duration in seconds
 breakdur = 5.0  # minimum duration of a break in seconds
 
 stimsize = 8  # size of stimuli in deg of visual angle
-repetitions = 2  # how many trials per condition
+repetitions = 1  # how many trials per condition
 
 screenrefresh = 60.0  # refresh rate of screen
-frequencies = [5.0, 7.5]  # the two (competing) frequencies
 
 simulationpercepts = [1.5, 3.5]  # interval for simulated percept length
 # durations will be sampled evenly from this interval
@@ -43,10 +42,6 @@ screen.setDistance(57)
 win = visual.Window([500, 500], allowGUI=True, monitor=screen,
                     units='deg', fullscr=True)
 
-# Start the keyboard monitor device
-io = launchHubServer()
-keyboard = io.devices.keyboard
-keyboard.reporting = True
 
 # Make the vergence cues (using list comprehension)
 square = visual.Circle(win, radius=np.sqrt(2) * stimsize / 2,
@@ -60,6 +55,9 @@ fixation = visual.GratingStim(win, tex="sqr", mask="cross", sf=0, size=0.5,
 message = visual.TextStim(win, units='norm', pos=[0, 0.75], height=0.06,
                           alignVert='center', alignHoriz='center',
                           text='')
+backreport = visual.TextStim(win, units='norm', pos=[0.9, -0.9], height=0.03,
+                             alignVert='center', alignHoriz='center',
+                             text='')
 
 
 # The arrays needed to construct the stimuli:
@@ -82,7 +80,7 @@ texlayer = [np.repeat(
 blanklayer = np.zeros(texlayer[0].shape) - 1
 
 # Make stimuli. This will be remade during each trial anyways tho
-stimuli = visual.RadialStim(win, size=stimsize, pos=fixations[stim].pos,
+stimuli = visual.RadialStim(win, size=stimsize, pos=fixation.pos,
                             mask=mask, colorSpace='rgb', angularRes=600,
                             # Radial and Angular Frequencies:
                             radialCycles=0.5,
@@ -99,10 +97,8 @@ def instruct(displaystring):
     message.text = displaystring + "\nPress [space] to continue."
     message.draw()
     win.flip()
-    keyboard.reporting = True
-    if 'escape' in keyboard.waitForReleases(keys=[' ', 'escape']):
+    if 'escape' in event.waitKeys(keyList=['space', 'escape']):
         raise KeyboardInterrupt
-    keyboard.reporting = False
 
 
 def trigger(value):
@@ -118,13 +114,8 @@ def demonstrate():
                     "the experiment, you would report seeing this by pressing "
                     "right.\nPress [space] to continue.")
     frame = 0
-    keyboard.reporting = True
-    while ' ' not in keyboard.getReleases(keys=[' ']):
-        frame += 1  # update frame tally
-        contrmod = [np.sign(np.cos(np.pi * frame / (screenrefresh /
-                                                    frequencies[layer])))
-                    for layer in range(2)]
-        stimuli.tex = np.dstack((texlayer[0] * contrmod[0],
+    while 'space' not in event.getKeys(keyList=['space']):
+        stimuli.tex = np.dstack((texlayer[0],
                                  blanklayer,
                                  blanklayer))
         message.draw()
@@ -132,8 +123,7 @@ def demonstrate():
         win.flip()
     # Clear & Flush
     win.flip()
-    keyboard.getKeys()
-    keyboard.reporting = False
+    event.getKeys()
     core.wait(1)
 
     # Draw Blue:
@@ -141,22 +131,16 @@ def demonstrate():
                     "In the experiment, you would report seeing this by "
                     "pressing left.\nPress [space] to continue.")
     frame = 0
-    keyboard.reporting = True
-    while ' ' not in keyboard.getReleases(keys=[' ']):
-        frame += 1  # update frame tally
-        contrmod = [np.sign(np.cos(np.pi * frame / (screenrefresh /
-                                                    frequencies[layer])))
-                    for layer in range(2)]
+    while 'space' not in event.getKeys(keyList=['space']):
         stimuli.tex = np.dstack((blanklayer,
                                  blanklayer,
-                                 texlayer[1] * contrmod[1]))
+                                 texlayer[1]))
         message.draw()
         stimuli.draw()
         win.flip()
     # Clear & Flush
     win.flip()
-    keyboard.getKeys()
-    keyboard.reporting = False
+    event.getKeys()
     core.wait(1)
 
     # Draw both rotating:
@@ -166,21 +150,15 @@ def demonstrate():
                     "either [left], [right], or [up]."
                     "\nPress [space] to continue.")
     frame = 0
-    keyboard.reporting = True
-    while not keyboard.getReleases(keys=[' ']):
-        frame += 1  # update frame tally
-        contrmod = [np.sign(np.cos(np.pi * frame / (screenrefresh /
-                                                    frequencies[layer])))
-                    for layer in range(2)]
-        stimuli.tex = np.dstack((texlayer[0] * contrmod[0],
+    while not event.getKeys(keyList=['space']):
+        stimuli.tex = np.dstack((texlayer[0],
                                  blanklayer,
-                                 texlayer[1] * contrmod[1]))
+                                 texlayer[1]))
         message.draw()
         stimuli.draw()
         win.flip()
     # Clear & Flush
     win.flip()
-    keyboard.reporting = False
     core.wait(1)
 
 
@@ -201,124 +179,54 @@ def rivaltrial(info):
                     "to begin the trial.")
     message.draw()
     win.flip()
-    keyboard.reporting = True
-    if "escape" in keyboard.waitForPresses(keys=['up', 'escape']):
+    if "escape" in event.waitKeys(keyList=['up', 'escape']):
         raise KeyboardInterrupt("You interrupted the script manually.")
 
     # This will keep track of response time:
     trialClock = core.Clock()
     win.callOnFlip(trialClock.reset)
 
-    # Make a list of contrast modulators to save time during trial
-    contrmod = [[(int(np.sign(np.cos(
-        np.pi * frame / (screenrefresh / info['frequency'][layer])))) + 1) / 2
-        for layer in range(2)]
-        for frame in range(int(trialdur * screenrefresh))]
-
-    # If this is a simulation trial, make a list to determine stimulus
-    stimsequence = np.zeros((int(trialdur * screenrefresh), 1),
-                            dtype=int) + 2
-    if info['simulation']:
-        perceptstart = 0
-        currentpercept = 0
-        while perceptstart < stimsequence.size:
-            perceptframes = int(screenrefresh * (np.random.random() *
-                                                 (simulationpercepts[1] -
-                                                  simulationpercepts[0]) +
-                                                 simulationpercepts[1]))
-            # assign the percept
-            if perceptstart + perceptframes < stimsequence.size:
-                stimsequence[
-                    perceptstart:(perceptstart + perceptframes)
-                ] = currentpercept
-                # update next percept start and value
-                currentpercept = (currentpercept + 1) % 2
-                perceptstart += perceptframes
-            else:
-                stimsequence[
-                    perceptstart:(stimsequence.size)
-                ] = currentpercept
-                perceptstart = stimsequence.size
-
-    # Pre-calculate all possible textures
-    textures = [
-        [[np.dstack((blanklayer,
-                     blanklayer,
-                     texlayer[info['pattern'][1]] *
-                     bluecontrast))
-          for redcontrast in [-1, 1]]
-         for bluecontrast in [-1, 1]],
-        [[np.dstack((texlayer[info['pattern'][0]] *
-                     redcontrast,
-                     blanklayer,
-                     blanklayer))
-          for redcontrast in [-1, 1]]
-            for bluecontrast in [-1, 1]],
-        [[np.dstack((texlayer[info['pattern'][0]] *
-                     redcontrast,
-                     blanklayer,
-                     texlayer[info['pattern'][1]] *
-                     bluecontrast))
-          for redcontrast in [-1, 1]]
-         for bluecontrast in [-1, 1]]
-    ]
-
-    # Make the textures
-    stimarray = [[[visual.RadialStim(win, size=stimsize,
-                                     pos=fixations[0].pos,
-                                     mask=mask, colorSpace='rgb',
-                                     angularRes=600,
-                                     # Radial and Angular Frequencies:
-                                     radialCycles=0.5,
-                                     angularCycles=1,
-                                     # Baseline Texture:
-                                     tex=textures[stim][red][blue]
-                                     )
-                   for red in range(2)]
-                  for blue in range(2)]
-                 for stim in range(3)]
-
-    # Now cycle through frames (actual trial):
-    trigger(100 + trials.thisN)
     for frame in range(int(trialdur * screenrefresh)):
 
         # Update the stimulus texture
-        stimarray[
-            stimsequence[frame]
-        ][contrmod[frame][0]][contrmod[frame][1]].draw()
+        stimuli.tex = np.dstack((texlayer[0],
+                                 blanklayer,
+                                 texlayer[1]))
+        stimuli.draw()
         # Flip
         win.flip()
 
         # Check response & save to array
-        keyArray[frame, :] = [response in keyboard.state.keys()
+        currKeys = event.getKeys()
+        keyArray[frame, :] = [response in currKeys
                               for response in responses.keys()]
         timeArray[frame] = trialClock.getTime()
-
-        # if response is different, trigger EEG
-        if not np.array_equal(keyArray[frame, :], keyArray[frame - 1, :]):
-            # send value as decimal rep of binary vector
-            trigger(1 + sum(1 << i
-                            for i, b in enumerate(keyArray[frame, :])
-                            if b))
+        if keyArray[frame, 1]:
+            backreport.text = 'mixture'
+        elif keyArray[frame, 0]:
+            backreport.text = 'red'
+        elif keyArray[frame, 2]:
+            backreport.text = 'blue'
+        backreport.draw()
 
     # After trial is over, clear the screen
+    backreport.text = ''
     win.flip()
-    keyboard.reporting = False
 
     # Save the trial info:
-    w = csv.DictWriter(open(datadir +
-                            'trial-%03d-info.csv' %
-                            trials.thisN, 'w+'),
-                       info.keys())
-    w.writeheader()
-    w.writerow(info)
-    # Save the data:
-    np.savetxt(datadir + 'trial-%03d-data.csv' % trials.thisN,
-               np.hstack([timeArray, keyArray, stimsequence]), fmt='%3.5f',
-               delimiter=', ',
-               header=', '.join(['time'] +
-                                responses.values() +
-                                ['simulationsequence']))
+    # w = csv.DictWriter(open(datadir +
+    #                         'trial-%03d-info.csv' %
+    #                         trials.thisN, 'w+'),
+    #                    info.keys())
+    # w.writeheader()
+    # w.writerow(info)
+    # # Save the data:
+    # np.savetxt(datadir + 'trial-%03d-data.csv' % trials.thisN,
+    #            np.hstack([timeArray, keyArray, stimsequence]), fmt='%3.5f',
+    #            delimiter=', ',
+    #            header=', '.join(['time'] +
+    #                             responses.values() +
+    #                             ['simulationsequence']))
 
     # return the two arrays
     return keyArray, timeArray
@@ -328,16 +236,14 @@ def rivaltrial(info):
 def rivalbreak():
     fixation.autoDraw = False  # disable fixation
     # Display message
-    keyboard.reporting = True
     for sec in range(int(breakdur)):
         message.text = "Break for %d seconds." % (breakdur - sec)
         message.draw()
         win.flip()
         # Pause for a second (interrupt if key pressed)
-        if 'escape' in keyboard.waitForReleases(maxWait=1, keys=['escape']):
+        if event.waitKeys(maxWait=1, keyList=['escape']):
             # Escape stops script:
             raise KeyboardInterrupt("You interrupted the script manually.")
-    keyboard.reporting = False
     fixation.autoDraw = True  # enable fixation
     win.flip()
 
@@ -375,18 +281,16 @@ instruct("If you have any other questions, please ask the experimenter now. "
 # randomise trial sequence:
 trials = data.TrialHandler(
     [{'pattern': [0, 1] if pattern else [1, 0],
-      'frequency': frequencies if flicker else list(reversed(frequencies)),
-      'flickering': False if trialtype == 0 else True,
+      'flickering': False,
       'simulation': True if trialtype == 2 else False}
      for pattern in range(2)
-     for flicker in range(2)
-     for trialtype in range(3)],
+     for trialtype in range(2)],
     repetitions)
 
 # make a directory for data storage
-datadir = os.path.join(os.getcwd(), 'data',  sessionInfo['time'] +
-                       ' ' + sessionInfo['subject'], '')
-os.makedirs(datadir)
+# datadir = os.path.join(os.getcwd(), 'data',  sessionInfo[
+#                        'time'] + sessionInfo['subject'], '')
+# os.makedirs(datadir)
 
 # Loop through trials:
 for trial in trials:
@@ -397,5 +301,4 @@ for trial in trials:
 
 # Close everything neatly
 win.close()
-tracker.destroy()
 core.quit()
